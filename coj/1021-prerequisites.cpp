08b7c87ee425554a27a2674fdcc4948879a215e7
Yaismel Miranda <yaismel.miranda@gmail.com>
2015-10-26 23:39:44 -0400

    coj: 1021 Prerequisites

---
+++ coj/1021-prerequisites.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int k, m, cursos[101];
+
+int main() {
+	int curso, cat, kp; bool puedo;
+	scanf("%d", &k);
+	while (k!=0) {
+    scanf("%d", &m);
+		for (int i = 0; i < k; i++) scanf("%d", &cursos[i]);
+    puedo = true;
+		for (int i = 0; i < m; i++) {
+			scanf("%d%d", &kp, &cat); int cont = 0;
+			while (kp--) {
+				scanf("%d", &curso);
+				for (int j = 0; j < k; j++)
+					if (curso==cursos[j]) { cont++; break; }
+			}
+			if (cont < cat) puedo = false;
+		}
+		if (puedo)
+			printf("yes\n");
+		else
+			printf("no\n");
+    scanf("%d", &k);
+	}
+	return 0;
+}
