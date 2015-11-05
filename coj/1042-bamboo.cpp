9fb3c4c3580b3bf80cc78211341cb604525c042e
Yaismel Miranda <yaismel.miranda@gmail.com>
2015-11-05 09:32:27 -0500

    format

--- coj/1042-bamboo.cpp
+++ coj/1042-bamboo.cpp
@@ -2,15 +2,15 @@
 float num, h , b, prom;
 int t;
 int main() {
-  prom = 0.0;
+	prom = 0.0;
 	scanf("%d", &t)	;
 	for (int i = 0; i < t; i++) {
 		scanf("%f%f", &h, &b);
 		num = (h*h - b*b)/(2*h);
-    prom += num;
+		prom += num;
 		printf("%.1f\n", num);
 	}
-  prom /= t;
-  printf("%.1f\n", prom);
-  return 0;
+	prom /= t;
+	printf("%.1f\n", prom);
+	return 0;
 }
