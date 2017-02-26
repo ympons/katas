574a2f7e63b821bb3f90240f59037d80776c3f8a
Yaismel Miranda <yaismel.miranda@gmail.com>
2017-02-26 17:28:11 -0500

    hackerrank: Sock Merchant challenge

---
+++ hackerrank/domain/alg/implementation/sock-merchant.go
@@ -0,0 +1,24 @@
+package main
+import (
+    "fmt"
+    "io"
+    "os"
+    "bufio"
+)
+
+var in io.Reader = bufio.NewReader(os.Stdin)
+func main() {
+    var n int
+    fmt.Fscan(in, &n)
+    m := make(map[int]int)
+    var x int
+    for i:=0;i<n;i++{
+        fmt.Fscan(in, &x)
+        m[x]++
+    }
+    count := 0
+    for _, v := range m {
+        count += v/2
+    }
+    fmt.Println(count)
+}
