6e4248e1c1609a63e79720ab5e4654d51eb3aecd
Yaismel Miranda <yaismel.miranda@gmail.com>
2020-10-25 12:58:08 -0400

    leetcode: 2. Add Two Numbers - improved

--- leetcode/0002-add-two-numbers/add_two_numbers.go
+++ leetcode/0002-add-two-numbers/add_two_numbers.go
@@ -6,6 +6,26 @@ import "github.com/ympons/challenges/essentials"
 type ListNode = essentials.ListNode
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head, sum, c := new(ListNode), 0, 0
+	node := head
+	for l1 != nil || l2 != nil || c != 0 {
+		sum = c
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		node.Next = &ListNode{Val: sum % 10}
+		node = node.Next
+		c = sum / 10
+	}
+	return head.Next
+}
+
+func addTwoNumbersOld(l1 *ListNode, l2 *ListNode) *ListNode {
 	l := &ListNode{}
 	aux := l
 	c := 0
@@ -18,7 +38,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			s += l2.Val
 		}
 		c = s / 10
-		aux.Next = &ListNode{s % 10, nil}
+		aux.Next = &ListNode{Val: s % 10}
 		aux = aux.Next
 		if l1 != nil {
 			l1 = l1.Next
@@ -28,7 +48,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 	}
 	if c > 0 {
-		aux.Next = &ListNode{c, nil}
+		aux.Next = &ListNode{Val: c}
 	}
 	return l.Next
 }
