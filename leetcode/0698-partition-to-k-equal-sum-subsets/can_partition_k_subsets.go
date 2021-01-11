e69cf1969c5943af37f5acfc31e8e82563dac93f
Yaismel Miranda <yaismel.miranda@gmail.com>
2021-01-11 01:28:22 -0500

    leetcode: 698. Partition to K Equal Sum Subsets -- DFS

--- leetcode/0698-partition-to-k-equal-sum-subsets/can_partition_k_subsets.go
+++ leetcode/0698-partition-to-k-equal-sum-subsets/can_partition_k_subsets.go
@@ -1,56 +1,55 @@
 package leetcode
 
-var visited map[int]struct{}
-
 func canPartitionKSubsets(nums []int, k int) bool {
-	n := len(nums)
-	if n < k {
+	n, s, mx := len(nums), sum(nums...), max(nums...)
+	bucketSum := s / k
+	if n < k || s%k > 0 || mx > bucketSum {
 		return false
 	}
+	M := map[int]struct{}{}
 
-	sum, max := 0, 0
-	for _, x := range nums {
-		sum += x
-		if x > max {
-			max = x
+	var dfs func(int, int, int) bool
+	dfs = func(k, start, currentSum int) bool {
+		// a single bucket is always valid
+		if k == 1 {
+			return true
 		}
-	}
-
-	if sum%k > 0 {
-		return false
-	}
-
-	bucketSum := sum / k
-	if max > bucketSum {
-		return false
-	}
-
-	visited = map[int]struct{}{}
-
-	return helper(nums, k, 0, 0, bucketSum)
-}
-
-func helper(nums []int, k, start, currentSum, bucketSum int) bool {
-	// a single bucket is always valid
-	if k == 1 {
-		return true
-	}
-	// found a valid bucket, need to check the others k-1
-	if currentSum == bucketSum {
-		return helper(nums, k-1, 0, 0, bucketSum)
-	}
-	n := len(nums)
-	for i := start; i < n; i++ {
-		// if it hasn't been visited try adding it to the current bucket
-		newSum := currentSum + nums[i]
-		if _, ok := visited[i]; !ok && newSum <= bucketSum {
-			visited[i] = struct{}{}
+		// found a valid bucket, need to check the others k-1
+		if currentSum == bucketSum {
+			return dfs(k-1, 0, 0)
+		}
+		for i := start; i < n; i++ {
+			// skip if it is already visited or out of bound
+			if _, visited := M[i]; visited || currentSum+nums[i] > bucketSum {
+				continue
+			}
+			M[i] = struct{}{}
 			// continue building the current bucket
-			if helper(nums, k, i+1, newSum, bucketSum) {
+			if dfs(k, i+1, currentSum+nums[i]) {
 				return true
 			}
-			delete(visited, i)
+			delete(M, i)
+		}
+		return false
+	}
+
+	return dfs(k, 0, 0)
+}
+
+func sum(nums ...int) int {
+	s := 0
+	for _, v := range nums {
+		s += v
+	}
+	return s
+}
+
+func max(nums ...int) int {
+	mx := 0
+	for _, v := range nums {
+		if mx < v {
+			mx = v
 		}
 	}
-	return false
+	return mx
 }
